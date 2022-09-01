3bfba2068e34a66be53ccf7ba3383180a8ca996b
Eric Cheatham <eric@meroxa.io>
2022-09-01 16:02:11 -0500

    Add Confluent Cloud resource type

--- main.py
+++ main.py
@@ -6,29 +6,19 @@ from turbine.runtime import RecordList, Runtime
 logging.basicConfig(level=logging.INFO)
 
 
-def transform(records: RecordList) -> RecordList:
-    print(records)
-    return
-
-
 class App:
     @staticmethod
     async def run(turbine: Runtime):
         try:
 
-            source = await turbine.resources("uwupostgres")
+            source = await turbine.resources("pg")
 
             records = await source.records("customer_order")
 
-            turbine.register_secrets("PWD")
 
-            # transformed = await turbine.process(records, transform)
+            dest = await turbine.resources("datalake")
 
-            destination_webhook = await turbine.resources("webhook-dest")
-            destination_webhook_two = await turbine.resources("webhook-dest-two")
-
-            await destination_webhook.write(records, "collection_archive", {})
-            await destination_webhook_two.write(records, "collection_archive", {})
+            await dest.write(records, "whatever",  {"file.name.template": "{{timestamp:unit=yyyy}}/{{timestamp:unit=MM}}/{{timestamp:unit=DD}}/{{timestamp:unit=hh}}/{{timestamp:unit=mm}}/file.gz"})
 
         except Exception as e:
             print(e, file=sys.stderr)
